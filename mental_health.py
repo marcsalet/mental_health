ce5f4f496021a4261b46f4f253928ac8cecf93ff
Marc Salet <marc.salet00@gmail.com>
2025-07-04 11:36:17 +0200

    Initial commit: full project

---
+++ mental_health.py
@@ -0,0 +1,14 @@
+import duckdb
+import pandas as pd
+
+db = duckdb.connect(database=':memory:')
+
+db.execute("create table data as select * from read_csv_auto('data/students_mental_health.csv')")
+
+data = db.execute("select * from data").fetchall()
+
+df = pd.read_csv('data/students_mental_health.csv')
+
+
+col = db.execute("PRAGMA table_info('data')").fetchdf()
+print(col)
