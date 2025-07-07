ee2042fc66b08f50ec4b285bc867e1846a4284ca
Marc Salet <ton.email@exemple.com>
2025-07-07 21:49:57 +0200

    transformation des tables sales (servant d'exemples) avec des noms plus adaptés

--- load.py
+++ load.py
@@ -6,6 +6,6 @@ def load_csv(uploaded_file):
     return df
 
 def load_to_duckdb(df):
-    con = duckdb.connect(database='sales_data.duckdb', read_only=False)
-    con.execute("CREATE OR REPLACE TABLE sales AS SELECT * FROM df")
+    con = duckdb.connect(database='data.duckdb', read_only=False)
+    con.execute("CREATE OR REPLACE TABLE data AS SELECT * FROM df")
     return con
